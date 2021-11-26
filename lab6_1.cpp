fca3d19b73987dfd225d229da223cbfb6bea9101
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-11-26 17:37:58 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+
+    int N;
+    int odd = 0;
+    int even = 0;
+    while ( N != 0)
+    {
+        cout << "Enter an integer:";
+        cin >> N;
+            if(N%2 == 0){
+                even += 1; 
+            if(N == 0){
+                even --;
+            } 
+            }else{
+                odd ++;
+            }
+    
+    }
+
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
+
     return 0;
 }
